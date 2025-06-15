b9fca6099eeab99a4ebca419e1ad1a6f0045e804
Toygun Bektasoglu <toygunbektasoglu@gmail.com>
2025-06-15 21:20:07 +0300

    feat: axios updated

--- client/src/libs/axios.ts
+++ client/src/libs/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_REACT_APP_URL}/`,
+  baseURL: `${import.meta.env.VITE_REACT_APP_URL}/api/v1`,
   withCredentials: true,
 });
 
--- server/src/app.ts
+++ server/src/app.ts
@@ -20,14 +20,14 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const PORT = process.env.PORT || 5000;
 
-app.use("/", issueRoutes);
+app.use("/api/v1", issueRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../client/dist")));
+  app.use(express.static(path.resolve(__dirname, "../../client/dist")));
 
   app.get("*", (req, res) => {
-    const indexPath = path.resolve(__dirname, "../client/dist/index.html");
+    const indexPath = path.resolve(__dirname, "../../client/dist/index.html");
     res.sendFile(indexPath);
   });
 }
