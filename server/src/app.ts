c5d6f90a4a2b7d36f81bef29df769f2bdfac38ef
Toygun Bektasoglu <toygunbektasoglu@gmail.com>
2025-06-15 20:49:31 +0300

    feat: deploy code

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import issueRoutes from "./routes/issue.routes";
-
+import path from "path";
+const __dirname = path.resolve();
 dotenv.config();
 
 const app = express();
@@ -20,6 +21,16 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 const PORT = process.env.PORT || 5000;
 
 app.use("/", issueRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    const indexPath = path.resolve(__dirname, "../client/dist/index.html");
+    res.sendFile(indexPath);
+  });
+}
 
 
 app.listen(PORT, () => {
